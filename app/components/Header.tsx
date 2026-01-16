c23baf240ae1ed78e5224e97b5bb1a66a6114bf8
Chenhao Lyu <ericlyu1122@gmail.com>
2026-01-16 00:53:48 -0500

    fit into mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,7 @@ import { Avatar, Name, Address, Identity } from '@coinbase/onchainkit/identity';
 
 export function Header() {
   return (
-    <nav className="h-16 border-b-4 border-black/40 flex items-center justify-between px-8 bg-[#121212] z-40">
+    <nav className="h-16 border-b-4 border-black/40 flex items-center justify-between px-4 md:px-8 bg-[#121212] relative z-50">
       <div className="flex items-center space-x-4">
         <Gamepad2 className="w-8 h-8 text-[#D64545] accent-glow-red" />
         <h1 className="pixel-font text-lg tracking-tight soft-text-shadow text-[#e5e5e5]">
